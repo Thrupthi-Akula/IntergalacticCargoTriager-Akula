8509ba4b53754c03e4af3ad5172260f7d24614de
thrupthiakula <thrupthi.akula@fastcollab.com>
2026-04-29 15:46:30 +0530

    Added API and screenshot for Task 2

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Load JSON data
+import json
+with open("Task 1 - Akula - Parser.json", "r") as f:
+    cargo_data = json.load(f)
+
+@app.route("/api/cargo", methods=["GET"])
+def get_cargo():
+    
+    # Business Rule 3
+    if request.headers.get("X-System-Override") == "true":
+        return "System override denied.", 418
+
+    return jsonify(cargo_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
